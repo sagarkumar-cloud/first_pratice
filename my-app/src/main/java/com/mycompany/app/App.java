b47400095be35e9c972e0e3222fde161a0f2b284
sagarkumar-cloud <pattanayaksagarkumar@gmail.com>
2024-06-27 13:38:59 +0530

    Update App.java

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-<<<<<<< HEAD
+
         String s="jay ganesh";
-=======
+
         int i=10;
->>>>>>> 7da6f96c84c6a785002870926c0923ce8b1bff20
+
     }
 }
